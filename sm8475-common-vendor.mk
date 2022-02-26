51d723ea3e05abb3f68a042f51e4e7f52be4f9f7
Arian <arian.kulmer@web.de>
2022-02-26 13:10:03 +0100

    sm8475-common: Set ro.media.xml_variant.profiles to choose  media profiles
    This was introduced with [1] and allows to choose target specific media
    profiles without caf changes to frameworks/av.
    
    [1]: https://github.com/LineageOS/android_frameworks_av/commit/eaac82f33f2a064c6ae1a5f1791bb13669820687#
    
    Change-Id: I78392c3f2f621a0e3f8301018c4a828c7259b849

--- sm8475-common-vendor.mk
+++ sm8475-common-vendor.mk
@@ -157,7 +157,6 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/sm8475-common/proprietary/vendor/etc/media_cape/video_system_specs.json:$(TARGET_COPY_OUT_VENDOR)/etc/media_cape/video_system_specs.json \
     vendor/motorola/sm8475-common/proprietary/vendor/etc/media_codecs_cape.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_cape.xml \
     vendor/motorola/sm8475-common/proprietary/vendor/etc/media_codecs_performance_cape.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_cape.xml \
-    vendor/motorola/sm8475-common/proprietary/vendor/etc/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
     vendor/motorola/sm8475-common/proprietary/vendor/etc/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     vendor/motorola/sm8475-common/proprietary/vendor/etc/media_profiles_cape.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_cape.xml \
     vendor/motorola/sm8475-common/proprietary/vendor/etc/models/acd/event.eai:$(TARGET_COPY_OUT_VENDOR)/etc/models/acd/event.eai \
